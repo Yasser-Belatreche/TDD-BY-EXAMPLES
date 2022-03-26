e388480a28c51cbb7c0c97ad27e373ea451f5279
Yasser-Belatreche <yasser.belatreche0@gmail.com>
2022-03-26 23:02:05 +0100

    add franc

--- src/MoneyExample/Dollar.java
+++ src/MoneyExample/Dollar.java
@@ -1,13 +1,20 @@
 package MoneyExample;
 
 public class Dollar {
-  int amount;
+  private int amount;
 
   Dollar(int amount) {
     this.amount = amount;
   }
 
-  void times(int multiplier) {
-    amount *= multiplier;
+  Dollar times(int multiplier) {
+    return new Dollar(amount * multiplier);
+  }
+
+  public boolean equals(Object object) {
+    Dollar dollar = (Dollar) object;
+
+    return dollar.amount == amount;
+
   }
 }
--- src/MoneyExample/DollarTest.java
+++ src/MoneyExample/DollarTest.java
@@ -2,14 +2,27 @@ package MoneyExample;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;;
+import static org.junit.Assert.*;
 
 public class DollarTest {
 
   @Test
-  public void should_multiply() {
+  public void testMultiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
+  }
+
+  @Test
+  public void testEqual() {
+    assertTrue(new Dollar(5).equals(new Dollar(5)));
+    assertFalse(new Dollar(5).equals(new Dollar(6)));
+  }
+
+  @Test
+  public void testFrancMultiplication() {
+    Franc five = new Franc(5);
+    assertEquals(new Franc(10), five.times(2));
+    assertEquals(new Franc(15), five.times(3));
   }
 }

6c99f583252c2fdf383305a063314859417b8ab2
Yasser-Belatreche <yasser.belatreche0@gmail.com>
2022-03-28 22:58:50 +0100

    remove Dollar and Franc classes

---
+++ src/MoneyExample/Money.java
@@ -0,0 +1,34 @@
+package MoneyExample;
+
+public class Money {
+
+  protected int amount;
+  protected String currency;
+
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  public boolean equals(Object object) {
+    Money money = (Money) object;
+    return amount == money.amount && currency().equals(money.currency());
+  }
+
+  static Money dollar(int amount) {
+    return new Money(amount, "USD");
+  }
+
+  static Money franc(int amount) {
+    return new Money(amount, "CHF");
+  }
+
+  String currency() {
+    return currency;
+  }
+
+  Money times(int multiplier) {
+    return new Money(amount * multiplier, currency);
+  }
+
+}
---
+++ src/MoneyExample/MoneyTest.java
@@ -0,0 +1,29 @@
+package MoneyExample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MoneyTest {
+
+  @Test
+  public void testMultiplication() {
+    Money five = Money.dollar(5);
+    assertEquals(Money.dollar(10), five.times(2));
+    assertEquals(Money.dollar(15), five.times(3));
+  }
+
+  @Test
+  public void testEqual() {
+    assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+    assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+    assertFalse(Money.franc(5).equals(Money.dollar(5)));
+  }
+
+  @Test
+  public void testCurrency() {
+    assertEquals("USD", Money.dollar(1).currency());
+    assertEquals("CHF", Money.franc(1).currency());
+  }
+
+}
